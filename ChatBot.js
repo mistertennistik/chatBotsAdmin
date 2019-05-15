4f381cc032c849f0986f8fb0c843be850a6358ed
tglauss <tglauss@enssat.fr>
2019-05-15 15:24:41 +0200

    init commit2

--- ChatBot.js
+++ ChatBot.js
@@ -1,1 +1,17 @@
-class 
+class ChatBot{
+
+	constructor(nom, cerveau){
+		this.nom = nom;
+		this.cerveau = cerveau;
+		this.interfaces = new Map();
+	}
+
+	addInterfaces(interface){
+		this.interfaces.push(interface);
+	}
+
+	delInterface(interface){
+		let ind = this.interfaces.indexOf(interface);
+	}
+
+}
