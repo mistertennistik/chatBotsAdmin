a5836dd0903b1bb61f9f102622ebef1741072a14
tglauss <tglauss@enssat.fr>
2019-05-20 16:46:40 +0200

    API REST OK

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Accès aux données //
+// Accès aux données ////////////////////////////////////
 
 const Chatbots = require('./Chatbots.js');
 
@@ -9,8 +9,112 @@ const client = new Discord.Client();
 
 
 var chatbots = new Chatbots();
-
+/*
 console.log("<<<<< chatbots issus de la BDD >>>>>>");
 console.log(chatbots);
 console.log("<<<<<<<<<<<-------------->>>>>>>>>>>>")
-console.log(chatbots.getChatBot(1));
+console.log(chatbots.getChatBot(1));*/
+
+////////////////////////////////////////////////////////
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+//var dao = require('dummyTagsDAO'); // in the node_modules directory
+
+
+
+
+
+var app = express();
+
+//https://expressjs.com/en/resources/middleware/cors.html
+app.use(cors());
+var corsOptions = {
+  origin: 'http://localhost:3030',
+  methods: 'GET,POST,PUT,DELETE',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/chatbots', cors(corsOptions),function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.json(chatbots.getChatBot());
+});
+
+app.get('/chatbot/:id', cors(corsOptions),function(req, res) {
+
+    let chatbot = chatbots.getChatBot(parseInt(req.params.id))
+    if(undefined!=chatbot)
+    {
+        res.setHeader('Content-Type', 'application/json');
+		    res.json(chatbot);
+    }
+    else
+    {
+		    res.send(404, 'Page introuvable !');
+	  }
+});
+
+app.post('/chatbot', cors(corsOptions), function(req, res) {
+
+    if(req.is('json')) //on devrait toujours tester le type et aussi la taille!
+    {
+		    var chatbot = chatbots.addChatBot(req.body);
+        res.setHeader('Content-Type', 'application/json');
+        res.json(chatbot);
+        console.log("Done adding "+JSON.stringify(chatbot) );
+	  }else{
+      res.send(400, 'Bad Request !');
+    }
+
+});
+app.put('/chatbot/:id', cors(corsOptions), function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    if(req.is('json')) //on devrait toujours tester le type et aussi la taille!
+    {
+		    var chatbot = chatbots.updateChatBot(req.body);
+        if(undefined==chatbot){
+          res.send(404, 'Page introuvable !');
+        }else{
+        	res.json(chatbot);
+        	console.log("Done updating "+JSON.stringify(chatbot) );
+	}
+    }else{
+      res.send(400, 'Bad Request !');
+    }
+
+});
+
+app.delete('/chatbot/:id', cors(corsOptions),function(req, res) {
+    let id = chatbots.deleteChatBot(parseInt(req.params.id));
+	console.log("delete "+id+" "+req.params.id+" hop");
+    if(undefined!=id){
+        res.setHeader('Content-Type', 'application/json');
+	res.send(200,'OK');
+    }else{
+	res.send(404, 'Page introuvable !');
+	}
+});
+
+
+
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(404, 'Page introuvable !');
+});
+
+app.listen(8082, (err,data) => {
+	if(err==undefined){
+		console.log(`app listening on port ${8082}!`);
+	}else{
+		console.log(`ERROR : ${err}`);
+	}
+    
+  });
