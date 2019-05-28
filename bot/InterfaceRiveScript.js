090b483b284be36354714bffadebd43b472c45b7
jrmdel <jdeletra@enssat.fr>
2019-05-28 15:21:02 +0200

    interface saved

--- bot/InterfaceRiveScript.js
+++ bot/InterfaceRiveScript.js
@@ -1,5 +1,35 @@
+var RiveScript = require('rivescript');
+var rs = new RiveScript();
+
+
 class InterfaceRiveScript(){
 	
 
-	
+	constructor(brain){
+		// brain est une string de la forme "./brain1.rive"
+		// c'est le cerveau qui va être utilisé
+		rs.loadFile(brain).then(loading_done).catch(loading_error);
+
+		function loading_done (batch_num) {
+			console.log("RIVE has finished loading!");
+			rs.sortReplies();
+		}
+
+		function loading_error (error) {
+			console.log("Error when loading files: " + error);
+		}
+	}
+
+
+	answer(user, message){
+		return rs.reply(user, message);
+	}
+
+
 }
+
+
+
+
+
+module.exports = InterfaceRiveScript;
